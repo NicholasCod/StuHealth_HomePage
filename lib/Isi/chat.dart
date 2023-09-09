1fe7741a3822f71d2435b4ee9f8d940b6c354c7c
NicholasCod <nicholas.inti@gmail.com>
2023-09-09 22:08:30 +0700

    first commit

---
+++ lib/Isi/chat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HalamanChat extends StatelessWidget {
+  const HalamanChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text('Chat Page'),
+    ));
+  }
+}
